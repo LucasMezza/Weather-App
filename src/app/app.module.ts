672a8e85df0afe3a53420566bad481e2067c809f
LucasMezza <mezza.lucas12@gmail.com>
2023-01-23 18:01:41 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BodyComponent } from './components/body/body.component';
+import { ApplicationComponent } from './components/application/application.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    BodyComponent,
+    ApplicationComponent
   ],
   imports: [
     BrowserModule
