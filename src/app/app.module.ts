d7f05bf63d3efcf52afde092ad19b1bb5a0e2f4c
LucasMezza <mezza.lucas12@gmail.com>
2023-01-31 18:04:26 -0300

    finished frontEnd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { ApplicationComponent } from './components/application/application.component';
-import { AppRoutingModule } from './app-routing.module';
 import { CurrentWeatherComponent } from './components/current-weather/current-weather.component';
-import { ForecastComponent } from './components/forecast/forecast.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
@@ -15,14 +12,11 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
     AppComponent,
     NavComponent,
     FooterComponent,
-    ApplicationComponent,
     CurrentWeatherComponent,
-    ForecastComponent,
     PageNotFoundComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
